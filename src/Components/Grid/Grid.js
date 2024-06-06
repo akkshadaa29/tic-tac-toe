a1b1d94bc0ac34a6076eb08b72570f3a05958213
Akshada Tambe <akshada.tambe33@gmail.com>
2024-06-06 10:24:24 +0530

    tic-tac-toe basic setup

---
+++ src/Components/Grid/Grid.js
@@ -0,0 +1,22 @@
+import './Grid.css';
+
+function Grid()
+{
+    return (
+        <div>    
+        <div className='grid-container'>
+         <button className='grid-item'> </button>
+         <button class='grid-item' > </button>
+         <button class='grid-item'> </button>  
+         <button class='grid-item' > </button>
+         <button class='grid-item' > </button>
+         <button class='grid-item' > </button>  
+         <button class='grid-item' > </button>
+         <button class='grid-item'> </button>
+         <button class='grid-item' > </button>
+        </div> 
+        </div>
+    );
+  }
+  
+  export default Grid;
