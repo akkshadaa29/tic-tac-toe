92b56ef5401fac4f572130c17ffc27d09bc7e3d3
Akshada Tambe <akshada.tambe33@gmail.com>
2024-06-07 23:57:44 +0530

    logic added

--- src/App.js
+++ src/App.js
@@ -11,3 +11,13 @@ function App() {
 }
 
 export default App;
+/*
+1.set current=X
+if current is x set it to O.
+if current is O set it to x.
+
+
+every grid block is a button
+onclick--> print.and change state. take input the grid number and update that also.
+
+*/
