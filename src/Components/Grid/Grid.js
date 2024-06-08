d1ea96d6c70a69693245b51140d86ace89802503
Akshada Tambe <akshada.tambe33@gmail.com>
2024-06-08 23:52:33 +0530

    adding logic for displaying X or O

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -1,11 +1,23 @@
 import './Grid.css';
+import { useState } from 'react';
 
 function Grid()
 {
+    let [data, setData] = useState("O");
+    const displayXO = () => {
+        if(data==="O")
+            {
+                setData("X");
+            }
+        else
+        {
+            setData("O");
+        }            
+      }
     return (
         <div>    
         <div className='grid-container'>
-         <button className='grid-item'> </button>
+         <button className='grid-item'onClick={() => displayXO()}> </button>
          <button class='grid-item' > </button>
          <button class='grid-item'> </button>  
          <button class='grid-item' > </button>
